ad66155fdd82b5c6d41939e1be6c84f3dbfb42ee
harshil-padasala <harshilpadsala999@gmail.com>
2023-02-11 16:24:43 +0530

    Implemented RETRY and added configuration in application.yml file

--- src/main/java/com/user/service/controllers/UserController.java
+++ src/main/java/com/user/service/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.user.service.controllers;
 import com.user.service.entities.User;
 import com.user.service.services.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,10 @@ public class UserController {
 
     private final String CIRCUIT_BREAKER_NAME = "RATING_HOTEL_SERVICE";
 
+    private final String RETRY_NAME = "RATING_HOTEL_SERVICE_RETRY";
+
+    private int count = 0;
+
     // create user
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
@@ -29,16 +34,19 @@ public class UserController {
     // get user by id
     @GetMapping("/{userId}")
     @CircuitBreaker(name = CIRCUIT_BREAKER_NAME, fallbackMethod = "circuitBreakerFallBack")
+    @Retry(name = RETRY_NAME, fallbackMethod = "retryFallBack")
     public ResponseEntity<Object> getUserById(@PathVariable String userId) {
         User user = userService.getUser(userId);
+        System.out.println(++count + " --> // get user by id");
         return ResponseEntity.ok(user);
     }
 
-    // get user by id
     @GetMapping
-    @CircuitBreaker(name = CIRCUIT_BREAKER_NAME, fallbackMethod = "circuitBreakerFallBack")
+//    @CircuitBreaker(name = CIRCUIT_BREAKER_NAME, fallbackMethod = "circuitBreakerFallBackAllUser")
+    @Retry(name = RETRY_NAME, fallbackMethod = "retryFallBackAll")
     public ResponseEntity<List<User>> getAllUser() {
         List<User> listUser = userService.getAllUser();
+        System.out.println(++count + " --> // get users");
         return ResponseEntity.ok(listUser);
     }
 
@@ -55,7 +63,18 @@ public class UserController {
     }
 
     public ResponseEntity<Object> circuitBreakerFallBack(String userId, Exception e) {
-        return ResponseEntity.ok("Sorry, Service is DOWN. Please try again later");
+        return ResponseEntity.ok("Sorry, Service is DOWN. Please try again later...");
+    }
 
+    public ResponseEntity<Object> circuitBreakerFallBackAllUser(Exception e) {
+        return ResponseEntity.ok("Sorry, Service is DOWN. Please try again later...");
+    }
+
+    public ResponseEntity<Object> retryFallBack(String userId, Exception e) {
+        return ResponseEntity.ok("Sorry, Didn't get response. Please Retry...");
+    }
+
+    public ResponseEntity<Object> retryFallBackAll(Exception e) {
+        return ResponseEntity.ok("Sorry, Didn't get response. Please Retry...");
     }
 }
