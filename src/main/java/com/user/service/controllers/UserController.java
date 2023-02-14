72dd4d17e60ae916f22db65c56ad36f6c18de40f
harshil-padasala <harshilpadsala999@gmail.com>
2023-02-14 10:59:08 +0530

    Implemented Rate Limiter, Fixed Retry and Circuit Breaker Issuses

--- src/main/java/com/user/service/controllers/UserController.java
+++ src/main/java/com/user/service/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.user.service.controllers;
 import com.user.service.entities.User;
 import com.user.service.services.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,9 +19,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    private final String CIRCUIT_BREAKER_NAME = "RATING_HOTEL_SERVICE";
+    private final String CIRCUIT_BREAKER_NAME = "RATING_HOTEL_SERVICE_CIRCUIT_BREAKER";
 
     private final String RETRY_NAME = "RATING_HOTEL_SERVICE_RETRY";
+
+    private final String RATE_LIMITER = "RATING_HOTEL_SERVICE_RATE_LIMITER";
 
     private int count = 0;
 
@@ -33,17 +36,18 @@ public class UserController {
 
     // get user by id
     @GetMapping("/{userId}")
-    @CircuitBreaker(name = CIRCUIT_BREAKER_NAME, fallbackMethod = "circuitBreakerFallBack")
-    @Retry(name = RETRY_NAME, fallbackMethod = "retryFallBack")
+//    @CircuitBreaker(name = CIRCUIT_BREAKER_NAME, fallbackMethod = "circuitBreakerFallBack")
+//    @Retry(name = RETRY_NAME)
+    @RateLimiter(name = RATE_LIMITER)
     public ResponseEntity<Object> getUserById(@PathVariable String userId) {
+        System.out.println("Retry method is called " + count++ + " times.");
         User user = userService.getUser(userId);
-        System.out.println(++count + " --> // get user by id");
         return ResponseEntity.ok(user);
     }
 
     @GetMapping
 //    @CircuitBreaker(name = CIRCUIT_BREAKER_NAME, fallbackMethod = "circuitBreakerFallBackAllUser")
-    @Retry(name = RETRY_NAME, fallbackMethod = "retryFallBackAll")
+//    @Retry(name = RETRY_NAME)
     public ResponseEntity<List<User>> getAllUser() {
         List<User> listUser = userService.getAllUser();
         System.out.println(++count + " --> // get users");
@@ -66,15 +70,11 @@ public class UserController {
         return ResponseEntity.ok("Sorry, Service is DOWN. Please try again later...");
     }
 
-    public ResponseEntity<Object> circuitBreakerFallBackAllUser(Exception e) {
-        return ResponseEntity.ok("Sorry, Service is DOWN. Please try again later...");
-    }
-
     public ResponseEntity<Object> retryFallBack(String userId, Exception e) {
         return ResponseEntity.ok("Sorry, Didn't get response. Please Retry...");
     }
 
-    public ResponseEntity<Object> retryFallBackAll(Exception e) {
-        return ResponseEntity.ok("Sorry, Didn't get response. Please Retry...");
+    public ResponseEntity<Object> rateLimiterFallBack(String userId, Exception e) {
+        return ResponseEntity.ok("Sorry, Our service is getting too many requests around the world. Please, try again late...");
     }
 }
